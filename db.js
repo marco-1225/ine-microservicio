d34b241457c71e1dfa5776874d25567baa0759bd
marco-1225 <marcogalax30@gmail.com>
2025-05-20 17:49:03 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
 
 connection.connect((err) => {
   if (err) {
