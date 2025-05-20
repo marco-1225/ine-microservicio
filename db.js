cb86730ff9e0165a8152ff3371976f6c6992b5ee
marco-1225 <marcogalax30@gmail.com>
2025-05-20 00:52:23 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,19 +1,14 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect((err) => {
   if (err) {
-    console.error('❌ Error al conectar a la base de datos:', err.message);
-    return;
+    console.error('❌ Error al conectar a la base de datos:', err);
+  } else {
+    console.log('✅ Conectado a la base de datos MySQL en Railway');
   }
-  console.log('✅ Conectado a la base de datos MySQL');
 });
 
 module.exports = connection;
